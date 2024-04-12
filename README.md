57b22b22d0c15a58658e6fed222239fa5e8e6a5f
fred13kim <fred13kim@gmail.com>
2024-04-11 20:03:26 -0400

    add fpm processing setup from summer

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 3d-printed-Microscopes
+
+> HMC 3D Printed Microscope monorepo
+
+## Getting Started
+
+`3d-printed-Microscopes` is a monorepo consisting of various projects
+implemented onto the 3D printed microscope platform.
+
+## Ongoing Projects
+
+* FPM (Fourier Ptychographic Microscopy)
+
+## DATASET MANAGEMENT
+
+To keep repo organized. Put all captured data in `datasets/` directory
---
+++ process/fpm/fourierptychography.py
@@ -0,0 +1,276 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation as animation
+from decimal import Decimal, ROUND_HALF_UP
+from argparse import Namespace
+
+
+DEFAULT_CONFIG = {
+        "num_aperture" : .15,
+        "wavelength"   : 623e-9,
+        "spsize"       : 1.12e-6,
+        "led_dist"     : 60e-3,
+        "led_sep"      : 3.175e-3,
+        "led_num"      : 8,
+        "arraysize"    : 7,
+}
+
+
+
+class FourierPtychography():
+    def __init__(self):
+        """
+        Fourier Ptychographic Imaging Algorithm Constructor
+
+        Args:
+
+        Returns:
+            Nothing
+        """
+        try:
+            self.create_configuration(DEFAULT_CONFIG)
+        except Exception:
+            print(Exception)
+
+    def create_configuration(self, config={}):
+        self.config = config
+        return config
+
+
+    @staticmethod
+    def round_hu(number):
+        """
+        A better round function
+
+        Args:
+            number: a floating point number
+
+        Returns:
+            the number rounded to ones digit with half up
+        """
+        return int(Decimal(number).quantize(0, ROUND_HALF_UP)) 
+
+    @staticmethod
+    def get_LED_center(centerImage):
+        """
+        simple LED position finder in the image domain (only center LED should be on)
+
+        Args:
+            centerImage: image with center LED on
+
+        Returns:
+            xavg, yavg: center position of LED
+        """
+        [m,n] = centerImage.shape
+        x = []
+        for i in range(n):
+            if ((centerImage[int(m/2),i]+centerImage[int(m/2)-1,i])/2 > 255/2):
+                x.append(i)
+        xavg = int((x[0] + x[len(x)-1])/2)
+        y = []
+        for i in range(m):
+            if ((centerImage[i,xavg]+centerImage[i,xavg-1])/2 > 255/2):
+                y.append(i)
+        yavg = int((y[0] + y[len(y)-1])/2)
+        return xavg,yavg
+
+    @staticmethod
+    def make_spiral_seq(x,y,arraysize):
+        """
+        counterclockwise spiral sequence generator for led matrix where (0,0) is bottom left
+    
+        Args:
+            x: center x position of spiral
+            y: center y position of spiral
+            arraysize: number of used leds
+            matrixsize: number of total leds
+Returns:
+            seq: sequence of the indexes of leds
+    
+        """
+        idx = x + (arraysize-y-1)*arraysize
+        x = y = 0
+        dx = 0
+        dy = -1
+        seq = []
+        for i in range(arraysize**2):
+            if (-arraysize/2 < x <= arraysize/2) and (-arraysize < y <= arraysize/2):
+                seq.append(idx)
+            if x == y or (x < 0 and x == -y) or (x > 0 and y == 1-x):
+                dx, dy = -dy, dx
+            idx = idx + dx
+            idx = idx + arraysize*dy
+            x = x + dx
+            y = y + dy
+        return seq
+
+    def create_vectors(self):
+        """
+        Create K vectors for FPM setup
+
+        Args:
+            Nothing
+
+        Returns:
+            kx,ky: numpy arrays for kx and ky vectors
+        """
+        arraysize = self.config["arraysize"]
+        led_sep   = self.config["led_sep"]
+        led_dist  = self.config["led_dist"]
+
+        kx = np.zeros(arraysize**2)
+        ky = np.zeros(arraysize**2)
+
+        for i in range(arraysize):
+            kx[i*arraysize:arraysize*(i+1)] = np.arange(-(arraysize-1)/2*led_sep,arraysize/2*led_sep,led_sep)
+            ky[i*arraysize:arraysize*(i+1)] = ((arraysize-1)/2-i)*led_sep
+        kx = -np.sin(np.arctan(kx/led_dist))
+        ky = -np.sin(np.arctan(ky/led_dist))  
+        return kx,ky
+
+    def recover(self,seqlowres):
+        """
+        Recover the hi-res image
+        
+        Args:
+            seqlowres: numpy array of sequential low resolution images
+
+        Returns:
+            recoveredObject: "Stitched" high resolution image
+
+        """
+
+        ### define seqlowres and upsampling ratio ###
+
+        [numim,m1,n1] = seqlowres.shape
+        pratio        = 4
+        psize         = self.config["spsize"] / pratio
+        m = m1 * pratio; n = n1 * pratio
+
+        ### create k vectors ###
+        k0      = 2 * np.pi / self.config["wavelength"]
+        [kx,ky] = self.create_vectors()
+        kx      = k0 * kx               ;  ky = k0 * ky
+        dkx     = 2*np.pi / (psize * n) ; dky = 2*np.pi / (psize * m)
+
+        #setup low pass filter 
+        cutoffFrequency = self.config["num_aperture"] * k0
+        kmax = np.pi/self.config["spsize"]
+        kx2 = np.arange(-kmax,kmax+kmax/((n1-1)/2),kmax/((n1-1)/2))
+        ky2 = np.arange(-kmax,kmax+kmax/((m1-1)/2),kmax/((m1-1)/2))
+
+        if (kx2.size > n1) : kx2 = kx2[:-1]
+        if (ky2.size > m1) : ky2 = ky2[:-1]
+
+        [kxm,kym] = np.meshgrid(kx2,ky2)
+        CTF = ((kxm**2 + kym**2) < cutoffFrequency**2) #Coherent Transfer Function Filter
+
+        seq = self.make_spiral_seq(3,3,self.config["arraysize"])
+
+        recoveredObject = np.ones((m,n))
+        recoveredObjectFT = np.fft.fftshift(np.fft.fft2(recoveredObject))
+
+        frames = []
+
+
+        loop = 3
+        for tt in range(loop):
+            for i3 in range(numim):
+                i2 = int(seq[i3])
+                kxc = self.round_hu((n+1)/2+kx[i2]/dkx)
+                kyc = self.round_hu((m+1)/2+ky[i2]/dky)
+                kxl = self.round_hu((kxc-(n1-1)/2))
+                kxh = self.round_hu((kxc+(n1-1)/2))
+                kyl = self.round_hu(kyc-(m1-1)/2)
+                kyh = self.round_hu(kyc+(m1-1)/2)
+
+                lowResFT = (m1/m)**2 * recoveredObjectFT[kyl:kyh+1,kxl:kxh+1] * CTF
+                lowResIm = np.fft.ifft2(np.fft.ifftshift(lowResFT))
+                lowResIm = (m/m1)**2 * seqlowres[i2,:,:] * np.exp(1j * np.angle(lowResIm))
+                lowResFT = np.fft.fftshift(np.fft.fft2(lowResIm)) * CTF
+                recoveredObjectFT[kyl:kyh+1,kxl:kxh+1] = (1-CTF) * recoveredObjectFT[kyl:kyh+1,kxl:kxh+1] + lowResFT
+                if (tt == 0):
+                    #frames.append(np.log(np.abs(recoveredObjectFT)))
+                    frames.append(np.abs(np.fft.ifft2(np.fft.ifftshift(recoveredObjectFT))))
+                    #frames.append(np.log(np.abs(np.fft.fftshift(np.fft.fft2(seqlowres[i3])))))
+                    continue
+        
+        recoveredObject = np.fft.ifft2(np.fft.ifftshift(recoveredObjectFT))
+
+        n=11
+
+        # display results
+        plt.figure()
+        plt.set_cmap("gray")
+        plt.imshow(seqlowres[n])
+        cbar = plt.colorbar(fraction = 0.047*(seqlowres.shape[1]/seqlowres.shape[2]))
+        cbar.set_label("Amplitude [a.u.]")
+        plt.title("Input amplitude")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        plt.figure()
+        plt.imshow(abs(recoveredObject))
+        cbar = plt.colorbar(fraction = 0.047*(recoveredObject.shape[0]/recoveredObject.shape[1]))
+        cbar.set_label("Amplitude [a.u.]")
+        plt.title("Recovered amplitude")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        
+        plt.figure()
+        plt.imshow(np.angle(recoveredObject))
+        plt.title("Recovered phase")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        
+        
+        plt.figure()
+        plt.imshow(np.log(abs(recoveredObjectFT)))
+        plt.title("Recovered FT")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        plt.figure()
+        plt.imshow(np.log(np.abs(np.fft.fftshift(np.fft.fft2(seqlowres[n])))))
+        plt.title("Original FT")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        plt.figure()
+        plt.imshow(seqlowres[n])
+        plt.title("Corresponding LowResIm")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        plt.figure()
+        #plt.imshow(np.log(np.abs(frames[11])))
+        plt.imshow(CTF)
+        plt.title("CTF")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+        
+        fig, ax = plt.subplots()
+        ims = []
+        for i in range(len(frames)):
+            im = ax.imshow(frames[i], animated = True)
+            ims.append([im])
+        
+        plt.title("Recovered FT Animation")
+        ax = plt.gca()
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ani = animation.ArtistAnimation(fig,ims, interval=500, blit=True, repeat_delay = 0)
+        
+        plt.show()
+
+
+        return recoveredObject
