9f8f6b19c46ac49003b5b7a7f3c6f3172786cc74
fred13kim <fred13kim@gmail.com>
2024-04-12 04:25:31 -0400

    start hmscope args few args in mind: process     fpm     ?? capture     fpm     brightfield     darkfield

--- bin/hmscope.py
+++ bin/hmscope.py
@@ -3,3 +3,25 @@
 import os
 import sys
 import argparse
+
+def main(argv=None):
+    description = \
+            '''
+            Welcome to HMC 3D Printed Microscope tool!
+            '''
+    parser = argparse.ArgumentParser(
+            prog = 'hmscope',
+            description = description,
+            )
+
+    subparsers = parser.add_subparsers(dest = "operation")
+
+    parser_process = subparsers.add_parser('process', help = 'processes datasets')
+
+    args = parser.parse_args(argv)
+    if args.operation is None:
+        parser.print_help()
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
